b7936c75c63c84f0209ae9d5b9e00cc695631930
Ruben Voss <bekertshop@gmail.com>
2022-08-15 15:24:43 +0200

    frontend + score

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,10 @@
 class GamesController < ApplicationController
   def new
-
+    # makes an array with 10 random letters
+    @letters = (0...10).map { rand(65..90).chr }
   end
 
   def score
-
+    raise
   end
 end
